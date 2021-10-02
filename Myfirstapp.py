bf9fc9559425c445a86577935ba2980f06b327d9
ieezzo <39259814+ieezzo@users.noreply.github.com>
2021-10-02 14:30:18 +0800

    Update Myfirstapp.py

--- Myfirstapp.py
+++ Myfirstapp.py
@@ -1,99 +1,64 @@
-# Python3 program to merge sort of linked list 
-  
-# create Node using class Node. 
-class Node: 
-    def __init__(self, dataValue): 
-        self.dataValue = dataValue
-        self.nextNode = None
-  
-class LinkedList: 
-    def __init__(self): 
-        self.headNode = None
-      
-    # push new value to linked list 
-    # using append method 
-    def append(self, newValue): 
-          
-        # Allocate new node 
-        newNode = Node(newValue) 
-          
-        # if head is None, initialize it to new node 
-        if self.headNode is None: 
-            self.headNode = newNode 
-            return
-        currentNode = self.headNode
-        while currentNode.nextNode is not None: 
-            currentNode = currentNode.nextNode
-              
-        # Append the new node at the end 
-        # of the linked list 
-        currentNode.nextNode = newNode  
-          
-    def sortedMerge(self, leftNode, rightNode): 
-        resultNode = None
-          
-        # Base cases 
-        if leftNode == None: 
-            return rightNode 
-        if rightNode == None: 
-            return leftNode 
-              
-        # pick either a or b and recur.. 
-        if leftNode.dataValue <= rightNode.dataValue: 
-            resultNode = leftNode 
-            resultNode.nextNode = self.sortedMerge(leftNode.nextNode, rightNode) 
-        else: 
-            resultNode = rightNode 
-            resultNode.nextNode = self.sortedMerge(leftNode, rightNode.nextNode) 
-        return resultNode 
-      
-    def mergeSort(self, headNode): 
-          
-        # Base case if head is None 
-        if headNode == None or headNode.nextNode == None: 
-            return headNode
-  
-        # get the middle of the list  
-        middleNode = self.getMiddle(headNode) 
-        nexttomiddleNode = middleNode.nextNode
-  
-        # set the next of middle node to None 
-        middleNode.nextNode = None
-  
-        # Apply mergeSort on left list  
-        left = self.mergeSort(headNode) 
-          
-        # Apply mergeSort on right list 
-        right = self.mergeSort(nexttomiddleNode) 
-  
-        # Merge the left and right lists  
-        sortedlist = self.sortedMerge(left, right) 
-        return sortedlist  
-      
-    # Utility function to get the middle  
-    # of the linked list  
-    def getMiddle(self, headNode): 
-        if (headNode == None): 
-            return headNode
-  
-        slowNode = headNode 
-        fastNode = headNode 
-  
-        while (fastNode.nextNode != None and 
-               fastNode.nextNode.nextNode != None): 
-            slowNode = slowNode.nextNode
-            fastNode = fastNode.nextNode.nextNode
-              
-        return slowNode 
-          
-# Utility function to print the linked list  
-def printList(headNode): 
-    if headNode is None: 
-        print(' ') 
-        return
-    curr_node = headNode 
-    while curr_node: 
-        print(curr_node.dataValue, end = " ") 
-        curr_node = curr_node.nextNode
-    print(' ') 
-      
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+import time
+
+st.header("My first Python Web App")
+
+readme = st.checkbox("readme first")
+
+if readme:
+
+    st.write("""
+        This is a web app demo using [streamlit](https://streamlit.io/) library. It is hosted on [heroku](https://www.heroku.com/). You may get the codes via [github](https://github.com/richieyuyongpoh/myfirstapp)
+        """)
+
+    st.write ("For more info, please contact:")
+
+    st.write("<a href='https://www.linkedin.com/in/yong-poh-yu/'>Dr. Yong Poh Yu </a>", unsafe_allow_html=True)
+
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['line chart','map','T n C','Long Process'])
+
+
+if option=='line chart':
+    chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+elif option=='map':
+    map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+    st.map(map_data)
+
+elif option=='T n C':
+
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
+    if show:
+        st.write(pd.DataFrame({
+        'Intplan': ['yes', 'yes', 'yes', 'no'],
+        'Churn Status': [0, 0, 0, 1]
+        }))
+
+
+else:
+    'Starting a long computation...'
+
+    
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+
+    for i in range(100):
+   
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
